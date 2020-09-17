ab9db268acfddc07d701cf8f575fc3cb9315ac7a
Zach Nedwich <zach@znedw.com>
2020-09-17 20:28:14 +1000

    configurable newsboat url 🤠

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const fastify = require('fastify')({ logger: true })
 const fs = require('fs').promises
+require('dotenv').config()
 
 fastify.put('/sub/add', async (request, reply) => {
   const { channelId } = request.body
   if (channelId) {
-    console.log(`Got a channel to add: ${channelId}`)
+    fastify.log.info(`Got a channel to add: ${channelId}`)
     await fs.appendFile(newsboatUrlFile(), buildSubUrl(channelId))
     return { status: 'ok' }
   } else {
@@ -13,7 +14,7 @@ fastify.put('/sub/add', async (request, reply) => {
   }
 })
 
-const newsboatUrlFile = () => `${require('os').homedir()}/.newsboat/urls`
+const newsboatUrlFile = () => process.env.NEWSBOAT_URL_FILE || `${require('os').homedir()}/.newsboat/urls`
 
 const buildSubUrl = (channelId) => `\nhttps://www.youtube.com/feeds/videos.xml?channel_id=${channelId.trim()}\n`
 
@@ -22,6 +23,7 @@ const start = async () => {
   try {
     await fastify.listen(3000)
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    fastify.log.info(`url config file is ${newsboatUrlFile()}`)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 don't use this
 
-adds a button to youtube.com to send the current channel to your newsboat url config file (not configurable)
+adds a button to youtube.com to send the current channel to your newsboat url config file (not configurable) (update; is configurable, use the NEWSBOAT_URL_FILE env variable or dotenv file)
+
+run `yarn install` then `yarn start`
+
+install `{grease,tamper}monkey`, install `tube2newsboat.user.js`
+
+go to youtube video
+
+click `add to newsboat`
+
+????
+
+profit
+
+🤟
